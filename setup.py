e8f45f63642745f40c1291be05b274498382979c
qixiaojian310 <2499126484@qq.com>
2025-03-07 00:15:25 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="streamlit_patch",  # 这里是pip项目发布的名称
-    version="0.0.11",  # 版本号，数值大的会优先被pip
+    version="0.0.12",  # 版本号，数值大的会优先被pip
     keywords=("pip", "streamlit_patch"),
     description="A streamlit patch for markdown render",
     long_description="A streamlit patch for markdown render",
@@ -17,6 +17,7 @@ setup(
         "streamlit==1.40.1",  # 指定streamlit版本
         "streamlit-echarts==0.4.0",  # 指定streamlit-echarts版本
         "streamlit-float==0.3.5",  # 指定streamlit-float版本
+        "streamlit_js_eval==0.1.7",
         "pandas<=2.0.3",
         "numpy<=1.24.4",
     ],  # 这个项目需要的第三方库
--- streamlit_patch/calculator.py
+++ streamlit_patch/calculator.py
@@ -1,37 +1,53 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from streamlit_js_eval import streamlit_js_eval
 
 
 class EchartCalculator:
-    def __init__(self, data):
-        self.calc_container = st.container()
-        self.res_container = st.container()
+    def __init__(self):
+        if "show_res" not in st.session_state:
+            st.session_state["show_res"] = False
+        self.page_width = streamlit_js_eval(
+            js_expressions="parent.document.getElementsByClassName('stSidebar')[0].clientWidth",
+            key="WIDTH",
+            want_output=True,
+        )
+        with st.sidebar:
+            self.calc_container = st.container()
+            with self.calc_container:
+                self.res_container = st.container()
+
+    def _trigger_res(self):
+        print(st.session_state["show_res"])
+        st.session_state["show_res"] = not st.session_state["show_res"]
 
     def inject_content(self):
         with self.calc_container:
-            st.markdown(
-                """<style>
-            .stSlider [data-baseweb=slider]{
-                width: 400px;
-            }
-            </style>""",
-                unsafe_allow_html=True,
-            )
-
-            st.write("📝 Calculator")
+            st.write("📝 Calculator", self.page_width)
             price = st.slider("Price", 0, 130, 25)
             flat = st.slider("Flat rate", 0, 130, 25)
             profit = st.slider("Profit", 0, 130, 25)
-            st.button("Submit")
+            st.button(
+                "Submit",
+                on_click=self._trigger_res,
+            )
         self.calc_container.float(
-            f"left: 0px; bottom: 0; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); border-radius: 10px;padding: 30px; width: 400px;box-sizing:content-box;z-index:999992"
+            f"position:fixed; bottom: 0; width: {self.page_width}px; margin-bottom: 10px"
         )
 
-        with self.res_container:
-            chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-            st.line_chart(chart_data, width=600, use_container_width=False, height=400)
-            st.button("Close")
-        self.res_container.float(
-            f"background-color: white; left: 460px; bottom: 0%;transform: translate(0%,0%); box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); border-radius: 10px;box-sizing:content-box;padding: 30px; width: 600px; overflow: auto;height:450px;z-index:999992"
-        )
+        if (
+            "show_res" in st.session_state
+            and st.session_state["show_res"]
+            and not st.session_state[self.renderKey]
+        ):
+            with self.res_container:
+                chart_data = pd.DataFrame(
+                    np.random.randn(20, 3), columns=["a", "b", "c"]
+                )
+                st.line_chart(
+                    chart_data, width=600, use_container_width=False, height=400
+                )
+            self.res_container.float(
+                f"position: absolute; background-color: white; left: 100%; bottom: 0%; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); border-radius: 10px;box-sizing:content-box;padding: 30px; width: 600px; height:450px;z-index:999999;"
+            )
