da410b53ace81118669722db472353e77bba87b8
qixiaojian310 <2499126484@qq.com>
2025-03-26 22:07:50 +0800

    update

--- build/lib/streamlit_patch/rec_container.py
+++ build/lib/streamlit_patch/rec_container.py
@@ -15,7 +15,7 @@ class RecContainer:
             data = ["华夏策略", "华安安康灵活配置A", "易增回报A"]
             for item in data:
                 with st.container():
-                    col1, col2, col3 = st.columns([2,3,4])
+                    col1, col2, col3 = st.columns([2,5,2])
                     col1.markdown(item)
                     chart_data = search_by_product_name(
                         RecContainer.CCB_path,
@@ -28,10 +28,7 @@ class RecContainer:
                         x="date",
                         x_label=item,
                         y_label="盈利趋势",
-                        height=160,
-                        width=400,
-                        use_container_width=False,
                     )
-                    col3.checkbox("Submit")
+                    col3.checkbox("Submit", key=f"rec_{item}")
                     st.divider()
         rec_container.float("position: fixed; bottom: 150px; right: 50px; z-index: 9999; width: 4rem; height: 2rem;")
