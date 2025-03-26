6fe82118e7cc996b48f5de03443bc7887f0b2792
qixiaojian310 <2499126484@qq.com>
2025-03-26 16:38:30 +0800

    update

---
+++ build/lib/streamlit_patch/rec_container.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+from .db_connector import search_by_product_name
+
+
+class RecContainer:
+  
+    @staticmethod
+    def init(db_path, CCB_path):
+        RecContainer.CCB_path = CCB_path
+        RecContainer.db_path = db_path
+    def __init__(self):
+        rec_container = st.container()
+        with rec_container.popover("💡",use_container_width=True).container(height=350):
+            data = ["华夏策略", "华安安康灵活配置A", "易增回报A"]
+            for item in data:
+                with st.container():
+                    col1, col2, col3 = st.columns([2,3,4])
+                    col1.markdown(item)
+                    chart_data = search_by_product_name(
+                        RecContainer.CCB_path,
+                        RecContainer.db_path,
+                        item,
+                    )
+                    col2.line_chart(
+                        chart_data,
+                        y=["unit_net_worth", "acc_net_worth"],
+                        x="date",
+                        x_label=item,
+                        y_label="盈利趋势",
+                        height=160,
+                        width=400,
+                        use_container_width=False,
+                    )
+                    col3.checkbox("Submit")
+                    st.divider()
+        rec_container.float("position: fixed; bottom: 150px; right: 50px; z-index: 9999; width: 4rem; height: 2rem;")
--- streamlit_patch/__init__.py
+++ streamlit_patch/__init__.py
@@ -1,6 +1,7 @@
 from .markdown_handler import EchartsHandlerContainer
 from .calculator import EchartCalculator
 from .report_summary import EchartReportCalculator
+from .rec_container import RecContainer
 from streamlit_float import *
 
 float_init()
@@ -9,4 +10,5 @@ __all__ = [
     "EchartsHandlerContainer",
     "EchartCalculator",
     "EchartReportCalculator",
+    "RecContainer"
 ]
