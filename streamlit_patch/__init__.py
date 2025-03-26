1b21183259222d5de29b5706b748186d31fff6e8
qixiaojian310 <2499126484@qq.com>
2025-03-26 16:26:33 +0800

    update

--- streamlit_patch/__init__.py
+++ streamlit_patch/__init__.py
@@ -1,6 +1,9 @@
 from .markdown_handler import EchartsHandlerContainer
 from .calculator import EchartCalculator
 from .report_summary import EchartReportCalculator
+from streamlit_float import *
+
+float_init()
 
 __all__ = [
     "EchartsHandlerContainer",
